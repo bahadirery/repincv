4379ccfb339462adc4f9f172263c81ac7bcb92b7
Bahadir <115642717+bahadirery@users.noreply.github.com>
2023-10-11 18:59:42 +0200

    Delete delete_ds_store.sh

--- delete_ds_store.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Specify the directory to start from, or use the current directory by default
-DIR="${1:-.}"
-
-# Find and delete .DS_Store files
-find "$DIR" -name ".DS_Store" -type f -delete
-
-echo "Deleted all .DS_Store files from $DIR and its subdirectories."
